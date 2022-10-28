87600c66a637f2692f5e33d0ac9387a9f1a21200
rrghiker <chris.ryan.hart1@gmail.com>
2022-10-28 13:49:27 -0400

    finished exercise

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Pokedex from './Pokedex';
+import Pokecard from './Pokecard';
 
 
 function App() {
   return (
     <div className="App">
-      <Pokedex />
+      <div className="title">
+        <h1>Pokedex</h1>
+      </div>
+      <div>
+        <Pokedex />
+      </div> 
     </div>
   );
 }
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,23 +1,19 @@
 import React from 'react';
+import "./Pokecard.css"
 
-const defaultPokemon = [
-    {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
-    {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
-    {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
-    {id: 12, name: 'Butterfree', type: 'flying', base_experience: 178},
-    {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
-    {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
-    {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
-    {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
-  ];
 
-const Pokecard = ({pokemon=[{id: 4, name: 'Charmander', type: 'fire', base_experience: 62}]}) => {
-    return (<div>
-                <p>pokemon.name</p>    
-                <p>pokemon.type</p>    
-                <p>pokemon.base_experience</p>    
-        <div/>)
 
+const Pokecard = ({pokemon={id: 4, name: 'Charmander', type: 'fire', base_experience: 62}}) => {
+    return (
+        <div className="Pokecard">
+            <div className="Pokecard-container" key={pokemon.id}>
+                <p><b>{pokemon.name}</b></p>
+                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt=""/>    
+                <p>Type: {pokemon.type}</p>    
+                <p>EXP: {pokemon.base_experience}</p>    
+            </div>
+        </div>
+    );
 }
 
 export default Pokecard;
